52143438f45957397f49273d8e46d07cbc175c2e
Carlos Clemente <carlos.clemente.abc@gmail.com>
2025-10-10 21:24:16 +0200

    Version v1.0.0.0 	- Primera verson funcional

---
+++ FacturaQR/InsertaQR.cs
@@ -0,0 +1,59 @@
+﻿using PdfSharp.Pdf;
+using PdfSharp.Pdf.IO;
+using PdfSharp.Drawing;
+using QRCoder;
+using System.Drawing;
+
+namespace FacturaQR
+{
+    public class InsertaQR
+    {
+        public static void InsertarQrConTexto(
+            string rutaPdfOriginal,
+            string rutaPdfSalida,
+            string textoQr,
+            string textoArriba,
+            string textoAbajo,
+            double posX,
+            double posY,
+            double ancho,
+            double alto)
+        {
+            PdfDocument documento = PdfReader.Open(rutaPdfOriginal, PdfDocumentOpenMode.Modify);
+
+            using(QRCodeGenerator qrGenerator = new QRCodeGenerator())
+            using(QRCodeData qrCodeData = qrGenerator.CreateQrCode(textoQr, QRCodeGenerator.ECCLevel.Q))
+            using(QRCode qrCode = new QRCode(qrCodeData))
+            using(Bitmap qrBitmap = qrCode.GetGraphic(20))
+            {
+                XImage qrImage;
+                using(var ms = new System.IO.MemoryStream())
+                {
+                    qrBitmap.Save(ms, System.Drawing.Imaging.ImageFormat.Png);
+                    ms.Position = 0;
+                    qrImage = XImage.FromStream(ms);
+                }
+
+                foreach(PdfPage pagina in documento.Pages)
+                {
+                    XGraphics gfx = XGraphics.FromPdfPage(pagina);
+
+                    // Fuente para los textos
+                    XFont font = new XFont("Arial", 8, XFontStyle.Bold);
+
+                    // Texto encima del QR
+                    gfx.DrawString(textoArriba, font, XBrushes.Black, new XRect(posX, posY - 10, ancho, 10), XStringFormats.Center);
+
+                    // QR
+                    gfx.DrawImage(qrImage, posX, posY, ancho, alto);
+
+                    // Texto debajo del QR
+                    gfx.DrawString(textoAbajo, font, XBrushes.Black, new XRect(posX, posY + alto, ancho, 10), XStringFormats.Center);
+                }
+            }
+
+            documento.Save(rutaPdfSalida);
+        }
+
+    }
+}
---
+++ FacturaQR/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace FacturaQR
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            if(args.Length < 8)
+            {
+                Console.WriteLine("Uso: FacturaQR <pdfOriginal> <pdfSalida> <textoQr> <textoArriba> <textoAbajo> <posX> <posY> <ancho> <alto>");
+                return;
+            }
+
+            try
+            {
+                string pdfOriginal = args[0];
+                string pdfSalida = args[1];
+                string textoQr = args[2];
+                string textoArriba = args[3];
+                string textoAbajo = args[4];
+                double posX = double.Parse(args[5]);
+                double posY = double.Parse(args[6]);
+                double ancho = double.Parse(args[7]);
+                double alto = double.Parse(args[8]);
+
+                InsertaQR.InsertarQrConTexto(pdfOriginal, pdfSalida, textoQr, textoArriba, textoAbajo, posX, posY, ancho, alto);
+
+                Console.WriteLine($"QR insertado correctamente en '{pdfSalida}'");
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine("Error al insertar el QR: " + ex.Message);
+            }
+        }
+
+
+    }
+}
